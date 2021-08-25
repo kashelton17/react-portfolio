18d1b2405b162002e2ce14d81b288b77375dfe6e
kashelton17 <kashelton17@gmail.com>
2021-08-25 16:53:13 -0500

    addign validation to form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,14 @@
 export default function Footer() {
-    return <footer className='content footer'>Made by Katie Shelton</footer>
+    return (
+        <div>
+            <footer className='content footer'>Made by Katie Shelton</footer>
+            <ul>
+                <li className='contact git'>
+                    <a href="github.com/kashelton17" target='_blank' rel="noreferrer">GitHub</a>
+                    <a href='https://www.linkedin.com/in/katie-shelton-67115365/' target="_blank" rel="noreferrer">LinkedIn</a>
+                </li>
+            </ul>
+        </div>
+        
+    )
 }
--- src/projectData/projectData.js
+++ src/projectData/projectData.js
@@ -2,7 +2,7 @@ const projectData = [
     {id: 1, title: 'About Me', html: (
         <section className='content one' id='about-me'>
             <h2>About Me</h2>
-            <p className='subcontent'>I graduated from Bucknell University with a Bachelor of Science in Chemistry and Bachelor of Arts in German.
+            <p className='aboutmecontent'>I graduated from Bucknell University with a Bachelor of Science in Chemistry and Bachelor of Arts in German.
                 I am currently working for a small tech company in New Hampshire as well as completing a web development certificate program part-time.
                 I am passionate about learning new skills and problem solving as well as running, spending time with family and being outdoors.
             </p>
@@ -10,7 +10,7 @@ const projectData = [
     ), route: ''},
     {id: 2, title: 'Work', html: (
         <section className='content two' id='work'>
-            <h2>Work</h2>
+            <h1 className='workHeader'>Work</h1>
             <div className='work'> 
                 <div className='workcontent'>
                     <div className='subcontent workone'>
@@ -33,20 +33,20 @@ const projectData = [
                     </div>
                     <div className='subcontent workfour'>
                         <div>
-                            <a className='worktitle' href="https://kashelton17.github.io/06-hw-weather-dashboard/" target='_blank' rel="noreferrer">Weather Dasboard</a>
-                            <a className='github-link' href="https://github.com/kashelton17/06-hw-weather-dashboard" target="_blank" rel="noreferrer">Repository</a>
+                            <a className='worktitle' href="https://budget-tracker-hw-16.herokuapp.com/" target='_blank' rel="noreferrer">Budget Tracker</a>
+                            <a className='github-link' href="https://github.com/kashelton17/16-hw-progressive-budget" target="_blank" rel="noreferrer">Repository</a>
                         </div>
                     </div>
                     <div className='subcontent workfive'>
                         <div>
-                            <a className='worktitle' href="https://kashelton17.github.io/06-hw-weather-dashboard/" target='_blank' rel="noreferrer">Weather Dasboard</a>
-                            <a className='github-link' href="https://github.com/kashelton17/06-hw-weather-dashboard" target="_blank" rel="noreferrer">Repository</a>
+                            <a className='worktitle' href="https://grooming-scheduler.herokuapp.com/" target='_blank' rel="noreferrer">Grooming Scheduler</a>
+                            <a className='github-link' href="https://github.com/Project-2-grooming-app/groomingScheduler" target="_blank" rel="noreferrer">Repository</a>
                         </div>
                     </div>
                     <div className='subcontent worksix'>
                         <div>
-                            <a className='worktitle' href="https://kashelton17.github.io/06-hw-weather-dashboard/" target='_blank' rel="noreferrer">Weather Dasboard</a>
-                            <a className='github-link' href="https://github.com/kashelton17/06-hw-weather-dashboard" target="_blank" rel="noreferrer">Repository</a>
+                            <a className='worktitle' href="https://secure-lake-33180.herokuapp.com/" target='_blank' rel="noreferrer">Note Taker</a>
+                            <a className='github-link' href="https://github.com/kashelton17/10-hw-note-taker" target="_blank" rel="noreferrer">Repository</a>
                         </div>
                     </div>
                 </div>
@@ -59,13 +59,13 @@ const projectData = [
             <div className='message-form'>
                 <form >
                     <label htmlFor='name-input'>Name: </label>
-                    <input type='text' id='name-input' placeholder='Name' />
+                    <input type='text' id='name-input' placeholder='Name' required/>
                     
                     <label htmlFor='email-input'>Email: </label>
-                    <input type='text' id='email-input' placeholder='Email'/>
+                    <input type='email' id='email-input' placeholder='Email' required/>
                     
-                    <label htmlFor='message-input'>Mesage: </label>
-                    <input type='text' id='message-input' placeholder='Message' size='200'/>
+                    <label htmlFor='message-input'>Message: </label>
+                    <input type='text' id='message-input' placeholder='Message' size='200' required/>
                     <input id='submit-form' type='submit' />
                 </form>
             </div>
